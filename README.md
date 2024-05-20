be88949b3af7e07da5f3d7057888d02b5ef9524e
mbaranturkmen <kilexey@gmail.com>
2024-05-20 20:33:13 +0100

    init: drizzle-kit studio for local db

--- README.md
+++ README.md
@@ -1,23 +1,37 @@
 # Getting started
 
+## Prerequisites
+
+1. Node.js >=v20.11.0
+2. pnpm >=v8.15.4
+
+## Initialise the database(s)
+
 For dev mode you need to:
 
 1. Create a D1 Database https://developers.cloudflare.com/d1/get-started/#3-create-a-database
 2. Create a `.env` file and a `wrangler.toml` file with the necessary information.
 3. Find and replace all "TBA" and "nextjs-d1-drizzle-cloudflare-pages" values in the code.
-4. Generate db migration files:
+4. Generate db migration files (which are SQL queries that will be run on the databases to update
+   their tables in the next step):
 
 ```sh
 pnpm db:generate
 ```
 
-4. Run db migrations locally:
+4. Run db migrations:
 
-```sh
-pnpm db:migrate:local
-```
+- local db: `pnpm db:migrate:local`
+- prod remote db: `pnpm db:migrate:prod:remote`
 
-5. Run Next.js on dev:
+5. View the database using a graphical user interface:
+
+- local db: `db:studio:local`
+- prod remote db: `db:studio:prod`
+
+## Run the app
+
+- Run Next.js on dev. Ideal for development since it supports hot-reload/fast refresh.
 
 ```sh
 pnpm dev
@@ -26,13 +40,18 @@ pnpm dev
 ⚠️ **Warning**: `next start` will return an error due to how the application is designed to run on
 Cloudflare pages.
 
-6. Run pages locally:
+- Run Cloudflare Pages locally. Ideal to test how the app would work after being deployed.
 
 ```sh
 pnpm pages:dev
 ```
 
-7. Deploy code to pages:
+⚠️ **Warning**: As of 20 May 2024, connecting to the prod remote db on the local code
+[is not supported](https://developers.cloudflare.com/d1/build-with-d1/local-development/).
+
+## Deploy
+
+- Deploy code to pages:
 
 ```sh
 pnpm pages:deploy
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,31 +1,31 @@
-// import { defineConfig, type Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-// import { env } from '@/env';
+import { env } from '@/env';
 
-// /*
-//  * NOTE: Workaround to make drizzle studio work with D1.
-//  * https://kevinkipp.com/blog/going-full-stack-on-astro-with-cloudflare-d1-and-drizzle/
-//  * Github discussion: https://github.com/drizzle-team/drizzle-orm/discussions/1545#discussioncomment-8115423
-//  */
-// export default env.LOCAL_DB_PATH
-//   ? {
-//       schema: './src/server/db/schema.ts',
-//       driver: 'd1',
-//       dialect: 'sqlite',
-//       dbCredentials: {
-//         url: env.LOCAL_DB_PATH,
-//       },
-//     }
-//   : {
-//       schema: './src/server/db/schema.ts',
-//       out: './migrations',
-//       driver: 'd1',
-//       dialect: 'sqlite',
-//       dbCredentials: {
-//         wranglerConfigPath:
-//           new URL('wrangler.toml', import.meta.url).pathname + env.WRANGLER_CONFIG
-//             ? ` ${env.WRANGLER_CONFIG}`
-//             : '',
-//         dbName: env.DB_NAME!,
-//       },
-//     };
+/*
+ * NOTE: Workaround to make drizzle studio work with D1.
+ * https://kevinkipp.com/blog/going-full-stack-on-astro-with-cloudflare-d1-and-drizzle/
+ * Github discussion: https://github.com/drizzle-team/drizzle-orm/discussions/1545#discussioncomment-8115423
+ */
+export default env.LOCAL_DB_PATH
+  ? {
+      schema: './src/server/db/schema.ts',
+      driver: 'd1',
+      dialect: 'sqlite',
+      dbCredentials: {
+        url: env.LOCAL_DB_PATH,
+      },
+    }
+  : defineConfig({
+      schema: './src/server/db/schema.ts',
+      out: './migrations',
+      driver: 'd1',
+      dialect: 'sqlite',
+      dbCredentials: {
+        wranglerConfigPath:
+          new URL('wrangler.toml', import.meta.url).pathname + env.WRANGLER_CONFIG
+            ? ` ${env.WRANGLER_CONFIG}`
+            : '',
+        dbName: env.DB_NAME!,
+      },
+    });
